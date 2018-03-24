b1da6c7d5110f1bd2f84e3f4e1c7e02e48262111
Rolando Parado <roland@stampede-design.com>
2018-03-24 21:55:48 +0800

    apply styling on masonry gallery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,22 +5,11 @@
             closeOnClickOutside: false,
             closeOnClickLink   : false
         });
-        $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: (target.offset().top - 54)
-                    }, 1000, "easeInOutExpo");
-                    return false;
-                }
-            }
-        });
-        // Activate scrollspy to add active class to navbar items on scroll
-        $('body').scrollspy({
-            target: '#Header',
-            offset: 1
-        });
+
+        $('.mh').children().matchHeight();
+        $.fn.matchHeight._maintainScroll = true;
     });
 })(jQuery);
+$(window).resize(function() {
+    $.fn.matchHeight._update() // Update MatchHeight.js
+});
